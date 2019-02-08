2590577794dfab145f1db9c7f7c6226ab3696a66
Alex Carmichael <carm0079@algonquinlive.com>
2019-02-07 22:05:55 -0500

    Added Validation to Dialog Box

--- src/components/PlayGround/PlayGround.js
+++ src/components/PlayGround/PlayGround.js
@@ -1,5 +1,33 @@
 export default {
   name: 'PlayGround',
+  methods: {
+    confirm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (false) {
+          return false
+        } else if (valid) {
+          // This is where user says yes or no
+          this.$confirm('Are you sure?', 'Confirm', {
+            distinguishCancelAndClose: true,
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No'
+          })
+            .then(() => {
+              this.$message({
+                type: 'info',
+                message: 'Your claim request has been sent!'
+              })
+            })
+            .catch(action => {
+              this.$message({
+                type: 'info',
+                message: action === 'cancel' ? 'Oh okay...' : 'Moving on.'
+              })
+            })
+        }
+      })
+    }
+  },
   data() {
     return {
       // Form data. Holds all content written by the user.
@@ -13,36 +41,48 @@ export default {
         agree: false
       },
       rules: {
-        firstName: [{
-          required: true,
-          message: 'Your first name is required',
-          trigger: 'blur'
-        }],
-        lastName: [{
-          required: true,
-          message: 'Your last name is required',
-          trigger: 'blur'
-        }],
-        email: [{
-          required: true,
-          message: 'Your email is required',
-          trigger: 'blur'
-        }],
-        subject: [{
-          required: true,
-          message: 'A subject is required',
-          trigger: 'blur'
-        }],
-        content: [{
-          required: true,
-          message: 'Some content is required',
-          trigger: 'blur'
-        }],
-        agree: [{
-          required: true,
-          message: 'You have to agree to the terms & conditions',
-          trigger: 'change'
-        }]
+        firstName: [
+          {
+            required: true,
+            message: 'Your first name is required',
+            trigger: 'blur'
+          }
+        ],
+        lastName: [
+          {
+            required: true,
+            message: 'Your last name is required',
+            trigger: 'blur'
+          }
+        ],
+        email: [
+          {
+            required: true,
+            message: 'Your email is required',
+            trigger: 'blur'
+          }
+        ],
+        subject: [
+          {
+            required: true,
+            message: 'A subject is required',
+            trigger: 'blur'
+          }
+        ],
+        content: [
+          {
+            required: true,
+            message: 'Some content is required',
+            trigger: 'blur'
+          }
+        ],
+        agree: [
+          {
+            required: true,
+            message: 'You have to agree to the terms & conditions',
+            trigger: 'blur'
+          }
+        ]
       },
       // The default visibility of the dialog box.
       dialogVisible: false
--- src/components/PlayGround/PlayGround.vue
+++ src/components/PlayGround/PlayGround.vue
@@ -36,7 +36,7 @@
       <span>Submit Request Form?</span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
+        <el-button type="primary" @click="confirm('form'), dialogVisible= false">Confirm</el-button>
       </span>
     </el-dialog>
   </div>
